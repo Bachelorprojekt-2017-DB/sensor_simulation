7b10d5e79e8229968d343b0c33813b6e4ec740a2
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-20 18:22:38 +0100

    Adapt graph to fit naming

--- src/util/graph.py
+++ src/util/graph.py
@@ -1,70 +1,72 @@
 # Graph class
-# Use getOrCreateVertex(stop_id, stop_name) for vertices
-# Use getOrCreateEdge(start_stop_id, end_stop_id) for edges
+# Use getOrCreateStation(stop_id, stop_name) for stations
+# Use getOrCreateEdge(start_stop_id, end_stop_id) for sections
 
 class Graph:
 	def __init__(self):
-		self.edges = []
-		self.vertices = []
+		self.sections = []
+		self.stations = []
 		self.highest_id = 0
 
-	def getOrCreateVertex(stop_id, stop_name):
-		vertex = self.vertex(stop_id)
-		if not vertex.isValid():
-			vertex = _Vertex(stop_id, stop_name)
-			if not vertex.isValid():
-				return vertex
-			self.vertices.append(vertex)
-		return vertex
+	def getOrCreateStation(stop_id, stop_name):
+		station = self.station(stop_id)
+		if not station.isValid():
+			station = _Station(stop_id, stop_name)
+			if not station.isValid():
+				return station
+			self.stations.append(station)
+		return station
 
-	def getOrCreateEdge(start_stop_id, end_stop_id):
-		start_vertex = self.getOrCreateVertex(start_stop_id)
-		end_vertex = self.getOrCreateVertex(end_stop_id)
-		if not (start_vertex.isValid() and end_vertex.isValid()):
-			return _Vertex()
-		return self._getOrCreateEdge(start_vertex, end_vertex)
+	def getOrCreateSection(start_stop_id, end_stop_id):
+		start_station = self.getOrCreatestation(start_stop_id)
+		end_station = self.getOrCreatestation(end_stop_id)
+		if not (start_station.isValid() and end_station.isValid()):
+			return _Section()
+		return self._getOrCreateSection(start_station, end_station)
 
-	def _getOrCreateEdge(first_vertex, second_vertex):
-		edge = self.edge(first_vertex, second_vertex)
-		if not edge.isValid():
-			edge = _Edge(self.highest_id, first_vertex, second_vertex)
+	def _getOrCreateSection(first_station, second_station):
+		section = self.section(first_station, second_station)
+		if not section.isValid():
+			section = _Section(self.highest_id, first_station, second_station)
 			highest_id += 1
-			first_vertex.addIncident(second_vertex, edge)
-			second_vertex.addIncident(first_vertex, edge)
-			self.edges.append(edge)
-		return edge
+			first_station.addIncident(second_station, section)
+			second_station.addIncident(first_station, section)
+			self.sections.append(section)
+		return section
 
-	def _vertex(stop_id):
-		for v in vertices:
-			if (v.stop_id == stop_id):
-				return v
-		return Vertex()
+	def _station(stop_id):
+		for station in stations:
+			if (station.stop_id == stop_id):
+				return station
+		return _Station()
 
-	def _edge(first_vertex, second_vertex):
-		for e in edges:
-			if (e.first_vertex == start_stop_id and e.second_vertex == end_stop_id or
-				e.second_vertex == start_stop_id and e.first_vertex == end_stop_id):
+	def _section(first_station, second_station):
+		for section in sections:
+			if (e.first_station == first_station and e.second_station == second_station or
+				e.second_station == first_station and e.first_station == second_station):
 				return e
 		return Edge()
 
-# private Vertex class for Graph, only instantiate over Graph class
-class _Vertex:
+# private Station class for Graph, only instantiate over Graph class
+class _Station:
 	def __init__(self, stop_id, stop_name):
 		self.stop_id = stop_id
 		self.stop_name = stop_name
-		self.incidents = []
+		self.collected_data = {} # Hash: section id => timestamp when visited
+		self.incidents = {} # Hash: adjacent station id => section id
 
 	def isValid():
 		return False if (self.stop_id is None) else True
 
-	def addIncident(vertex, edge):
-		self.incidents.append([vertex, edge])
+	def addIncident(station, section):
+		self.incidents[station] = section
 
 # private Edge class for Graph, only instantiate over Graph class
-class _Edge:
-	def __init__(self, first_vertex, second_vertex):
-		self.first_vertex = first_vertex
-		self.second_vertex = second_vertex
+class _Section:
+	def __init__(self, section_id, first_station, second_station):
+		self.first_station = first_station
+		self.second_station = second_station
+		self.section_id = section_id
 
 	def isValid():
 		return False if (self.first_stop_id is None or self.second_stop_id is None) else True
