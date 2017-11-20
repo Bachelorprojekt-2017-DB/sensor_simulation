3cc31461914c643779980dbf3d6bb2e7ea8a5c7f
Melvin Witte <melvin.witte@student.hpi.de>
2017-11-20 17:59:18 +0100

    Change function names to camelCase

--- src/util/graph.py
+++ src/util/graph.py
@@ -1,6 +1,6 @@
 # Graph class
-# Use get_or_create_vertex(stop_id, stop_name) for vertices
-# Use get_or_create_edge(start_stop_id, end_stop_id) for edges
+# Use getOrCreateVertex(stop_id, stop_name) for vertices
+# Use getOrCreateEdge(start_stop_id, end_stop_id) for edges
 
 class Graph:
 	def __init__(self):
@@ -8,29 +8,29 @@ class Graph:
 		self.vertices = []
 		self.highest_id = 0
 
-	def get_or_create_vertex(stop_id, stop_name):
+	def getOrCreateVertex(stop_id, stop_name):
 		vertex = self.vertex(stop_id)
-		if not vertex.is_valid():
+		if not vertex.isValid():
 			vertex = _Vertex(stop_id, stop_name)
-			if not vertex.is_valid():
+			if not vertex.isValid():
 				return vertex
 			self.vertices.append(vertex)
 		return vertex
 
-	def get_or_create_edge(start_stop_id, end_stop_id):
-		start_vertex = self.get_or_create_vertex(start_stop_id)
-		end_vertex = self.get_or_create_vertex(end_stop_id)
-		if not (start_vertex.is_valid() and end_vertex.is_valid()):
+	def getOrCreateEdge(start_stop_id, end_stop_id):
+		start_vertex = self.getOrCreateVertex(start_stop_id)
+		end_vertex = self.getOrCreateVertex(end_stop_id)
+		if not (start_vertex.isValid() and end_vertex.isValid()):
 			return _Vertex()
-		return self._get_or_create_edge(start_vertex, end_vertex)
+		return self._getOrCreateEdge(start_vertex, end_vertex)
 
-	def _get_or_create_edge(first_vertex, second_vertex):
+	def _getOrCreateEdge(first_vertex, second_vertex):
 		edge = self.edge(first_vertex, second_vertex)
-		if not edge.is_valid():
+		if not edge.isValid():
 			edge = _Edge(self.highest_id, first_vertex, second_vertex)
 			highest_id += 1
-			first_vertex.add_incident(second_vertex, edge)
-			second_vertex.add_incident(first_vertex, edge)
+			first_vertex.addIncident(second_vertex, edge)
+			second_vertex.addIncident(first_vertex, edge)
 			self.edges.append(edge)
 		return edge
 
@@ -54,10 +54,10 @@ class _Vertex:
 		self.stop_name = stop_name
 		self.incidents = []
 
-	def is_valid():
+	def isValid():
 		return False if (self.stop_id is None) else True
 
-	def add_incident(vertex, edge):
+	def addIncident(vertex, edge):
 		self.incidents.append([vertex, edge])
 
 # private Edge class for Graph, only instantiate over Graph class
@@ -66,5 +66,5 @@ class _Edge:
 		self.first_vertex = first_vertex
 		self.second_vertex = second_vertex
 
-	def is_valid():
+	def isValid():
 		return False if (self.first_stop_id is None or self.second_stop_id is None) else True
