a9c3d8a3cd8702106d39c5779994c47cca83f26b
Lukas Pirl <git@lukas-pirl.de>
2017-12-08 20:56:17 +0100

    cleanup ``Train.initialize_events``

--- src/train.py
+++ src/train.py
@@ -13,27 +13,21 @@ class Train:
 		stops.sort()
 
 		length = len(stops)
-		self.arrivals = list(range(length -1))
-		self.departures = list(range(length - 1))
-		self.on_section = list(range(length - 2))
+		self.arrivals = [None] * (length - 1)
+		self.departures = [None] * (length - 1)
+		self.on_section = [None] * (length - 2)
 
 		# temporary dictionary to speed up lookup
 		stop_time_by_seq = {t.stop_sequence: t for t in self.trip.stop_times}
 
-		# TODO: deduplicate the following two code blocks
-		# TODO: I feel this can be optimized
-
-		seq = stops[0]
-		stop_time = stop_time_by_seq[seq]
-		self.arrivals[0] = [stop_time.arrival_time, int(stop_time.stop_id)]
-		self.departures[0] = [stop_time.departure_time, int(stop_time.stop_id)]
-
-		for i in range(1, len(stops) - 1):
-			seq = stops[i]
+		previous_i = None
+		for i, seq in enumerate(stops[:-1]):
 			stop_time = stop_time_by_seq[seq]
 			self.arrivals[i] = [stop_time.arrival_time, int(stop_time.stop_id)]
 			self.departures[i] = [stop_time.departure_time, int(stop_time.stop_id)]
-			self.on_section[i-1] = [self.departures[i - 1], self.arrivals[i]]
+			if previous_i is not None:
+				self.on_section[previous_i] = (self.departures[previous_i], self.arrivals[i])
+			previous_i = i
 
 	def notify(self, train_location, time):
 		train_location.update(self.collected_data)
